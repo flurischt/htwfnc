02ba536e87b7e929983e48d3c46ca5f28fd09157
Flurin Rindisbacher <info@flurischt.ch>
2015-04-19 19:22:18 +0200

    assignment5, exercise 2 WIP

--- assignment5/skeleton/src/comp.c
+++ assignment5/skeleton/src/comp.c
@@ -34,11 +34,14 @@
 
 void simd_conjugate_transpose (complex_t in[2][2], complex_t out[2][2])
 {
+    //Load
     __m128 row1 = _mm_load_ps((float*)in[0]);
     __m128 row2 = _mm_load_ps((float*)in[1]);
     __m128 multiplier = _mm_set_ps(-1.0, 1.0, -1.0, 1.0);
+    //Compute
     row1 = _mm_mul_ps(row1, multiplier);
     row2 = _mm_mul_ps(row2, multiplier);
+    //Shuffle/Store
     __m128 conj_row = _mm_shuffle_ps(row1, row2, _MM_SHUFFLE(1, 0, 1, 0));
     _mm_store_ps((float*) out[0], conj_row);
     conj_row = _mm_shuffle_ps(row1, row2, _MM_SHUFFLE(3, 2, 3, 2));
@@ -47,7 +50,17 @@ void simd_conjugate_transpose (complex_t in[2][2], complex_t out[2][2])
 
 void simd_pairs_multiplications (float * x, float * y, float * z, size_t n)
 {
-	// your code goes here ...
+    size_t i;
+    for(i=0;i<=n-4;i+=4)
+    {
+        //Load two complex numbers from x and two from y
+        __m128 ab = _mm_load_ps(x+i); // LSB[a1, b1, a2, b2]
+        __m128 cd = _mm_load_ps(y+i); // LSB[c1, d1, c2, d2]
+    
+        //Compute
+        __m128 prod1 = _mm_mul_ps(ab, cd); // LSB[a1*c1, b1*d1, a2*c2, b2*d2]
+
+    }
 }
 
 void simd_ceil (float * m, size_t n)
